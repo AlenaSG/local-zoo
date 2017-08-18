5e89e7c70b24afa4d5caedf70c0ae1729d0dcb08
Alena Golovina <alenasgolovina@gmail.com>
2017-08-18 16:31:22 -0700

    add code to display dynamic data in template

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,17 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Local Zoo</h1>
     <h3>{{currentAnimals}}</h3>
+
+    <ul>
+       <li>{{firstAnimal.name}}</li>
+     </ul>
   </div>
   `
 })
 
 export class AppComponent {
 currentAnimals: string = 'See these animals today!';
+firstAnimal = {
+  name: "Tiger"
+}
 }
