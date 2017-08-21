b895c954be12b6562fe8938573d5a426e658526b
Alena Golovina <alenasgolovina@gmail.com>
2017-08-20 23:22:02 -0700

    add input and output and event emitter

---
+++ app/animal-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Animal } from './animal.model';
+
+@Component({
+  selector: 'animal-list',
+  template: `
+  <ul>
+     <li *ngFor="let currentAnimal of childAnimalList">{{currentAnimal.species}} <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></li>
+   </ul>
+  `
+})
+
+export class AnimalListComponent {
+  @Input() childAnimalList: Animal[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(animalToEdit: Animal) {
+      this.clickSender.emit(animalToEdit);
+    }
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,7 @@ import { Component } from '@angular/core';
     <h1>Local Zoo</h1>
     <h3>{{currentAnimals}}</h3>
 
-    <ul>
-       <li *ngFor="let currentAnimal of animals">{{currentAnimal.species}} <button (click)="editAnimal(currentAnimal)">Edit!</button></li>
-     </ul>
+ <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
 
      <hr>
 
@@ -39,14 +38,16 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-currentAnimals: string = 'See these animals today!';
-animals: Animal[] = [
+currentAnimals: string = 'See these animals today!'
+
+selectedAnimal = null;
+
+
+masterAnimalList: Animal[] = [
   new Animal ('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
   new Animal ('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
   new Animal ('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Femail', 'Delicate roots and leaves', 'Loud noises')
 ];
-
-selectedAnimal = null;
 
 editAnimal(clickedAnimal) {
   this.selectedAnimal = clickedAnimal;
@@ -55,9 +56,4 @@ editAnimal(clickedAnimal) {
 finishedEditing() {
     this.selectedAnimal = null;
   }
-}
-
-
-export class Animal {
-  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
 }
