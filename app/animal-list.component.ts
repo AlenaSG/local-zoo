2b098169835cabd8539c1fad91651e9931e43957
Alena Golovina <alenasgolovina@gmail.com>
2017-08-21 00:46:28 -0700

    add filtering with pipe by maturity

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,8 +4,14 @@ import { Animal } from './animal.model';
 @Component({
   selector: 'animal-list',
   template: `
+
+  <select>
+      <option value="allAnimals">All Animals</option>
+      <option value="youngAnimals">Young Animals</option>
+      <option value="matureAnimals" selected="selected">Mature Animals</option>
+    </select>
   <ul>
-     <li *ngFor="let currentAnimal of childAnimalList">{{currentAnimal.species}} <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></li>
+     <li *ngFor="let currentAnimal of childAnimalList | maturity:filterByMaturity">{{currentAnimal.species}} <button (click)="editButtonHasBeenClicked(currentAnimal)">Edit!</button></li>
    </ul>
   `
 })
@@ -14,7 +20,14 @@ export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
 
+
+filterByMaturity: string = "matureAnimals";
+
   editButtonHasBeenClicked(animalToEdit: Animal) {
       this.clickSender.emit(animalToEdit);
     }
+
+    onChange(optionFromMenu) {
+    this.filterByMaturity = optionFromMenu;
+  }
 }
